297f4053880d35cf8f8f2c2dfe79b7d9503c15e9
Dewa <adewalade@gmail.com>
2021-06-28 23:21:38 +0100

    chore: app cleanup for main todo app project

--- App.js
+++ App.js
@@ -4,97 +4,9 @@ import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native'
 
 export default function App() {
 
-  const [todos, setTodos] = useState([{
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 6,
-    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 7,
-    "title": "illo expedita consequatur quia in",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 8,
-    "title": "quo adipisci enim quam ut ab",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "molestiae perspiciatis ipsa",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 10,
-    "title": "illo est ratione doloremque quia maiores aut",
-    "completed": true
-  }])
-
-  const pressHandler = (id) => {
-    let returnValue = [...todos]
-    returnValue.forEach(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-    })
-    
-    setTodos(returnValue)
-  }
-
   return (
     <View style={styles.container}>
       <Text> Our Todo List </Text>
-      <FlatList 
-        numColumns={3}
-        keyExtractor={(item) => item.id.toString()}
-        data={todos}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={{
-              ...styles.todo, 
-              backgroundColor: item.completed ?
-              '#8699aa' : '#003f5c'
-              }}>
-              {item.title}
-            </Text>
-          </TouchableOpacity>
-        )}
-      />
       <StatusBar style="auto" />
     </View>
   );
