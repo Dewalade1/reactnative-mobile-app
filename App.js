7f78e813e3056b3f7ae5446eb3a2fe76fff5a797
Dewa <adewalade@gmail.com>
2021-06-28 22:15:55 +0100

    chore: folder cleanup

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Hello World! </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: 300,
+    height: 50,
+    padding: 10,
+    margin: 10,
+  },
+
+  myText: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+
+  buttonStyle: {
+    backgroundColor: 'green',
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
